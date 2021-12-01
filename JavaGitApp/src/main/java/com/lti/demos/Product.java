9c7b6a5ac0403ce3f5dc848437597235309a01fb
snehabiranige <snehabiranige@gmail.com>
2021-12-01 15:24:10 +0530

    commit third

--- JavaGitApp/src/main/java/com/lti/demos/Product.java
+++ JavaGitApp/src/main/java/com/lti/demos/Product.java
@@ -6,10 +6,14 @@ public class Product {
 	private int prdId;
 	private String prdName;
 	
-	public Product(int i, String string) {
-		
+	
+	public Product(int prdId, String prdName) {
+		super();
+		this.prdId = prdId;
+		this.prdName = prdName;
 	}
 
+	
 	@Override
 	public String toString() {
 		return "Product [prdId=" + prdId + ", prdName=" + prdName + "]";
