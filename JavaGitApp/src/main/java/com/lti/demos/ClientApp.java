1729bc848178729e94786b1f28f146d42ff62dc2
snehabiranige <snehabiranige@gmail.com>
2021-12-01 14:24:25 +0530

    first commit

---
+++ JavaGitApp/src/main/java/com/lti/demos/ClientApp.java
@@ -0,0 +1,12 @@
+package com.lti.demos;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+
+      Product p1=new Product(101,"Mobile");
+      System.out.println(p1);
+
+	}
+
+}
---
+++ JavaGitApp/src/main/java/com/lti/demos/Product.java
@@ -0,0 +1,25 @@
+package com.lti.demos;
+
+public class Product {
+
+	
+	private int prdId;
+	private String prdName;
+	
+	public Product(int i, String string) {
+		
+	}
+	public int getPrdId() {
+		return prdId;
+	}
+	public void setPrdId(int prdId) {
+		this.prdId = prdId;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+	
+}
